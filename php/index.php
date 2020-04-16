e705b5064d3e73b075220119bf7bdca71f5c230b
wgonzalez2005 <el.master46@gmail.com>
2020-04-15 23:17:15 -0400

    Creacion carpeta php

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos rerum ratione ab accusantium repellat dignissimos commodi sint iure quis consectetur at natus, quod sed vero est quam! Cupiditate, neque, rem?</p>
+</body>
+</html>
